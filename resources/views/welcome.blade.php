a5d31f456c4589872e5480e9e992883382c37041
ayauper <ayaup@qq.com>
2017-11-30 17:02:07 +0000

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My sample laravel!!
                 </div>
 
                 <div class="links">
