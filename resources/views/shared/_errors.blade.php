987dd35c6ea1058b0f0141559c4f1f628cabf2b5
ayauper <ayaup@qq.com>
2017-12-13 14:27:35 +0000

    /Finish user Signup

--- resources/views/shared/_errors.blade.php
+++ resources/views/shared/_errors.blade.php
@@ -1,4 +1,4 @@
-@if (count($errors) > 0)
+<!-- @if (count($errors) > 0)
   <div class="alert alert-danger">
       <ul>
           @foreach($errors->all() as $error)
@@ -6,4 +6,9 @@
           @endforeach
       </ul>
   </div>
+@endif -->
+
+
+@if (count($errors) > 0)
+    {{ count($errors) }}
 @endif
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.default')
 @section('content')
   <h1>帮助页</h1>
-@stop
+@stop@stop
